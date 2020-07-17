1b8484a56aab8647f42bb9db86e3a730ca94abf7
Onuchukwu Chika Augustine <chuzksy@yahoo.com>
2020-07-17 07:44:13 +0100

    Initial Comit of Infrastructure.Data project Install EF Core packages Create UniversityDbContext

---
+++ CleanArch/CleanArch.Infrastructure.Data/Context/UniviersityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infrastructure.Data.Context
+{
+    public class UniviersityDbContext : DbContext
+    {
+        public UniviersityDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        DbSet<Course> Courses { get; set; }
+    }
+}
